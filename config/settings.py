04a0800b22d1aafd218c31d67abe9b08d98b8514
Oneyura <hryn.yuri@gmail.com>
2025-04-15 20:03:18 +0300

    Add user model to settings

--- config/settings.py
+++ config/settings.py
@@ -101,6 +101,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "task_manager.Worker"
+
+LOGIN_REDIRECT_URL = "/"
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
