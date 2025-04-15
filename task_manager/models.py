7ff45cf662fdc7a785e48f2c0bee73ca2d5a04ae
Oneyura <hryn.yuri@gmail.com>
2025-04-15 20:01:49 +0300

    Create models v1
    Created models: Tag; TaskType; Position; Task; Worker(user).

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,45 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Task(models.Model):
+    class Priority(models.TextChoices):
+        LOW = "Low"
+        NORMAL = "Normal"
+        HIGH = "High"
+        URGENT = "Urgent"
+
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(blank=True, null=True)
+    status = models.BooleanField(default=False)
+    task_type = models.ForeignKey(TaskType, on_delete=models.SET_NULL, null=True)
+    tags = models.ManyToManyField(Tag, related_name="tasks")
+    assignees = models.ManyToManyField(get_user_model(), related_name="assigned_tasks")
+    priority = models.CharField(choices=Priority, default=Priority.NORMAL, max_length=10)
+
+
+    class Meta:
+        ordering = ["status", "-date"]
+        verbose_name = "Task"
+        verbose_name_plural = "Tasks"
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+
+
