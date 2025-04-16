eca328a78bbaee2229a23d070979191b03bf713a
Oneyura <hryn.yuri@gmail.com>
2025-04-16 22:16:33 +0300

    Add filter forms from Views to forms.py

--- task_manager/forms.py
+++ task_manager/forms.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 
-from task_manager.models import Task
+from task_manager.models import Task, Tag, TaskType, Position
 
 
 class TaskForm(forms.ModelForm):
@@ -28,16 +28,58 @@ class WorkerForm(UserCreationForm):
         fields = UserCreationForm.Meta.fields + ("position", )
 
 
-# class TaskSearchForm(forms.Form):
-#     name = forms.CharField(
-#         widget=forms.TextInput(attrs={"placeholder": "Search by name"}),
-#         label="",
-#         max_length=100,
-#         required=False,
-#     )
-#     tag = forms.CharField(
-#         widget=forms.TextInput(attrs={"placeholder": "Search by name"}),
-#         label="",
-#         max_length=100,
-#         required=False,
-#     )
+class TaskFilterForm(forms.Form):
+    STATUS_CHOICES = (
+        ("", "all"),
+        ("true", "Completed"),
+        ("false", "Not completed")
+    )
+
+    name = forms.CharField(
+        widget=forms.TextInput(attrs={"placeholder": "Search by name"}),
+        label="",
+        max_length=100,
+        required=False,
+    )
+    tag = forms.ModelMultipleChoiceField(
+        queryset=Tag.objects.all(),
+        required=False,
+        label="Tag",
+        widget=forms.CheckboxSelectMultiple,
+    )
+    task_type = forms.ModelMultipleChoiceField(
+        queryset=TaskType.objects.all(),
+        required=False,
+        label="Task Type",
+        widget=forms.CheckboxSelectMultiple,
+    )
+    status = forms.ChoiceField(
+        choices=STATUS_CHOICES,
+        required=False,
+        label="Status",
+    )
+    priority = forms.ChoiceField(
+        choices=[("", "All priorities")] + list(Task.Priority.choices),
+        required=False,
+        label = "Priority"
+    )
+    only_my = forms.BooleanField(
+        required=False,
+        label="Only my tasks",
+        widget=forms.CheckboxInput(attrs={"class": "form-check-input"})
+    )
+
+
+class WorkerFilterForm(forms.Form):
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={"placeholder": "Search by username"}),
+        label="",
+        max_length=100,
+        required=False,
+    )
+    position = forms.ModelMultipleChoiceField(
+        queryset=Position.objects.all(),
+        required=False,
+        label="Position",
+        widget=forms.CheckboxSelectMultiple,
+    )
