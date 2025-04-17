250f3fa94b0875bb28685ebb94c460b900d8764f
Oneyura <hryn.yuri@gmail.com>
2025-04-17 23:09:58 +0300

    add user frendly representation to models.py

--- task_manager/models.py
+++ task_manager/models.py
@@ -6,13 +6,22 @@ from django.contrib.auth.models import AbstractUser
 class Tag(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+
 
 class TaskType(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+
 
 class Position(models.Model):
     name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
 
 
 class Worker(AbstractUser):
@@ -49,3 +58,6 @@ class Task(models.Model):
         ordering = ["status", "-date"]# add priority
         verbose_name = "Task"
         verbose_name_plural = "Tasks"
+
+    def __str__(self):
+        return self.name
