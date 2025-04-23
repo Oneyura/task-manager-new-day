0e32394d9506c38eeabca2d2b38ba03ec70dda85
Oneyura <hryn.yuri@gmail.com>
2025-04-23 10:59:47 +0300

    Update admin panel

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from task_manager.models import (
+    Task,
+    Worker,
+    Tag,
+    Position,
+    TaskType
+)
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('name', 'deadline', 'status')
+    list_filter = ('status',)
+    search_fields = ('name',)
+
+
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Worker, UserAdmin)
+admin.site.register(Tag)
+admin.site.register(Position)
+admin.site.register(TaskType)
+
